724f65dbe80dc5f2683733fb7562ebfbee3b33ef
playstonex <32049715+playstonex@users.noreply.github.com>
2017-10-25 14:40:49 +0800

    Carthage (#3)
    * support carthage
    
    * carthage icon

---
+++ Mathematics-tvOsTests/Mathematics_tvOsTests.swift
@@ -0,0 +1,36 @@
+//
+//  Mathematics_tvOsTests.swift
+//  Mathematics-tvOsTests
+//
+//  Created by lei on 2017/10/25.
+//  Copyright © 2017年 lei. All rights reserved.
+//
+
+import XCTest
+@testable import Mathematics_tvOs
+
+class Mathematics_tvOsTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
